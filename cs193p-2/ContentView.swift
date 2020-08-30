52f2a682e2f947a209f3ae8188e889e8a0e94953
leechanwoo <cjswosa2@gmail.com>
2020-08-30 15:51:53 +0900

    init commit

---
+++ cs193p-2/ContentView.swift
@@ -0,0 +1,16 @@
+//
+//  ContentView.swift
+//  cs193p-2
+//
+//  Created by chanwoo on 2020/08/30.
+//  Copyright © 2020 chanwoo. All rights reserved.
+//
+
+import SwiftUI
+
+struct EmojiArtDocumentView: View {
+    @ObservedObject var document: EmojiArtDocument
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
---
+++ cs193p-2/EmojiArt.swift
@@ -0,0 +1,37 @@
+//
+//  EmojiArt.swift
+//  cs193p-2
+//
+//  Created by chanwoo on 2020/08/30.
+//  Copyright © 2020 chanwoo. All rights reserved.
+//
+
+import Foundation
+
+struct EmojiArt {
+    var backgroundURL: URL?
+    var emojis = [Emoji]()
+    
+    struct Emoji: Identifiable {
+        let text: String
+        var x: Int // offset from center
+        var y: Int // offset from center
+        var size: Int
+        let id: Int
+        
+        fileprivate init(text: String, x: Int, y: Int, size: Int, id: Int) {
+            self.text = text
+            self.x = x
+            self.y = y
+            self.size = size
+            self.id = id
+        }
+    }
+    
+    private var uniqueEmojiId = 0
+    
+    mutating func addEmoji(_ text: String, x: Int, y: Int, size: Int) {
+        uniqueEmojiId += 1
+        emojis.append(Emoji(text: text, x: x, y: y, size: size, id: uniqueEmojiId))
+    }
+}
